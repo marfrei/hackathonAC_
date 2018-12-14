9e2689042e00bb418045f258dd602c7da8d7b176
Luis <llopes.261199@gmail.com>
2018-12-14 01:49:30 +0000

    Controllers created

--- src/main/java/org/academiadecodigo/invictus/sinceramente/persistence/services/RatingService.java
+++
@@ -1,9 +0,0 @@
-package org.academiadecodigo.invictus.sinceramente.persistence.services;
-
-import org.academiadecodigo.invictus.sinceramente.persistence.model.Rating;
-
-public interface RatingService {
-
-    void save(Rating rating);
-    Rating get(Integer id);
-}
